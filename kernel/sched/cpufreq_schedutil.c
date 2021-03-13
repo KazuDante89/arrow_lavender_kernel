938cd432f62ea3151ceb614602d8a6fc7ee79446
Panchajanya1999 <panchajanya@azure-dev.live>
2021-03-13 13:45:06 +0700

    cpufreq: schedutil: Expose limit tunables cluster-wise
    This allows us to tune the default rate-limits for each cluster
    without constantly changing schedutil's code.
    
    Idea taken from : kdrag0n/proton_zf6@d9faa70#diff-d96c15a8e1af1ce4aa4750bef6b7e38d
    [panchajanya1999] : Allow tuning for each cluster
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit de92c2b)
    Change-Id: I9d36a99dddc2a7138f44336d4325930c28425220
    
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   kernel/sched/cpufreq_schedutil.c
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   kernel/sched/cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -16,7 +16,8 @@
 #include <linux/kthread.h>
 #include <linux/slab.h>
 #include <trace/events/power.h>
-
+#include <linux/sched/sysctl.h>
+#include <linux/binfmts.h>
 #include "sched.h"
 #include "tune.h"
 
@@ -506,6 +507,10 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -525,6 +530,10 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -772,19 +781,14 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	if (policy->up_transition_delay_us && policy->down_transition_delay_us) {
-		tunables->up_rate_limit_us = policy->up_transition_delay_us;
-		tunables->down_rate_limit_us = policy->down_transition_delay_us;
-	} else {
-		unsigned int lat;
+	if (cpumask_test_cpu(policy->cpu, cpu_perf_mask)) {
+		tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_BIG;
+		tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_BIG;
+	}
 
-                tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-                tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
-		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
-		if (lat) {
-                        tunables->up_rate_limit_us *= lat;
-                        tunables->down_rate_limit_us *= lat;
-                }
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask)) {
+		tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT_LITTLE;
+		tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT_LITTLE;
 	}
 
 	tunables->iowait_boost_enable = 1;
