854a98b4fa96da7a5d18b93a8f2c486a4e0c396b
KazuDante89 <karlemmanueldorvil@gmail.com>
2022-07-12 09:26:47 -0400

    ... 	modified:   arch/arm/boot/dts/qcom/sdm660-gpu.dtsi 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="Newbie_v03"
+ZIPNAME="Newbie_v04"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
