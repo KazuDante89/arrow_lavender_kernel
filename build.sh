11f2b174de9d55256a43684c9a674882e07bb2c1
KazuDante89 <karlemmanueldorvil@gmail.com>
2022-07-15 00:32:54 -0400

    Testing 	modified:   .ci.yml 	modified:   arch/arm/boot/dts/qcom/xiaomi/longcheer/common/display/dsi-panel-boe-td4320-1080p-video.dtsi 	modified:   arch/arm64/configs/lavender_defconfig 	modified:   build.sh 	modified:   include/linux/thermal.h

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ KERNEL_DIR="$(pwd)"
 BASEDIR="$(basename "$KERNEL_DIR")"
 
 # The name of the Kernel, to name the ZIP
-ZIPNAME="Neuron_FBR_v01"
+ZIPNAME="NeuronR1_v00"
 
 # Build Author
 # Take care, it should be a universal and most probably, case-sensitive
