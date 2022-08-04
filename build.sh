5698f7708667e3c046d0f8428f62d272bff749c8
KazuDante89 <karlemmanueldorvil@gmail.com>
2022-08-04 08:50:58 -0400

    testing neutron-16 	modified:   build.sh

--- build.sh
+++ build.sh
@@ -215,8 +215,8 @@ COMMIT_HEAD=$(git log --oneline -1)
 
 	if [ $COMPILER = "clang" ]
 	then
-    msg "|| Cloning Azure Clang ||"
-		git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang-llvm
+    msg "|| Cloning Neutron Clang 16 ||"
+		git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-16 clang-llvm
 		# Toolchain Directory defaults to clang-llvm
 		TC_DIR=$KERNEL_DIR/clang-llvm
 	fi
